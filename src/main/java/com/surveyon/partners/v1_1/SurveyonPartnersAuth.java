8f1af87ceab2e38e44dd948596d86ed55b0d0b0f
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2015-02-26 14:19:41 +0900

    add javadoc

--- src/main/java/com/surveyon/partners/v1_1/SurveyonPartnersAuth.java
+++ src/main/java/com/surveyon/partners/v1_1/SurveyonPartnersAuth.java
@@ -1,3 +1,7 @@
+/**
+* Copyright (C) Research Panel Asia, Inc.
+* 
+**/
 package com.surveyon.partners.v1_1;
 
 import java.security.InvalidKeyException;
@@ -11,22 +15,55 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Hex;
 
+/**
+ * Authentication helper for surveyob Partenrs API v1.1 
+ * @author research panel asia
+ */
 public class SurveyonPartnersAuth {
+	
+	/**
+	 * query with signature generated from given parameters
+	 */
 	private String query = null;
+	
+	/**
+	 * signature generated from given parameters
+	 */
 	private String signature = null;
 
+	/**
+	 * getter
+	 * @return signature
+	 */
 	public String getSignature() {
 		return signature;
 	}
 
+	/**
+	 * getter
+	 * @return query
+	 */
 	public String getQuery() {
 		return query;
 	}
 
+	/**
+	 * Create SurveyonPartnersAuth.
+	 * @param parameters in TreeMap
+	 * @param app secret
+	 * @throws Exception
+	 */
 	public SurveyonPartnersAuth(TreeMap params, String secret) throws Exception {		
 		this.generateSignature(params, secret);
 	}
-
+	
+	/**
+	 * Verify given signature is valid 
+	 * @param parameters in TreeMap
+	 * @param app secret
+	 * @return
+	 * @throws Exception
+	 */
 	public static boolean verifySignature(TreeMap params, String secret) throws Exception{
 		if (!params.containsKey("sig") || params.get("sig") == null){
 			throw new Exception("sig doesn't exist");
@@ -43,6 +80,13 @@ public class SurveyonPartnersAuth {
 		return created.equals(signature);
 	}
 
+	/**
+	 * Generate signature from given parameters and app secret
+	 * @param parameters in TreeMap
+	 * @param app secret
+	 * @return generated signature
+	 * @throws Exception
+	 */
 	private String generateSignature(TreeMap params, String secret) throws Exception {
 		//create HTTP parameter string
 		String combined = SurveyonPartnersAuth.createCombinedParameter(params);
@@ -56,6 +100,12 @@ public class SurveyonPartnersAuth {
 		return sig;
 	}
 
+	/**
+	 * Combine given parameters as query string ordered alphabetically
+	 * @param params
+	 * @return query string
+	 * @throws Exception
+	 */
 	private static String createCombinedParameter(TreeMap params) throws Exception {
 		StringBuilder builder = new StringBuilder();
 		for (Iterator it = params.entrySet().iterator(); it.hasNext();) {
@@ -71,8 +121,16 @@ public class SurveyonPartnersAuth {
 		return builder.toString();
 	}
 
+	/**
+	 * Create HMAC Sha256 Hex
+	 * @param message
+	 * @param key
+	 * @return HMAC Sha256 Hex
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeyException
+	 */
 	private static String creatHmacSha256Hex(String message, String key) throws NoSuchAlgorithmException, InvalidKeyException {
-		SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(),"HmacMD5");
+		SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(),"HmacSHA256");
 		Mac mac = Mac.getInstance("HmacSHA256");
 		mac.init(keySpec);
 		byte[] rawHmac = mac.doFinal(message.getBytes());
