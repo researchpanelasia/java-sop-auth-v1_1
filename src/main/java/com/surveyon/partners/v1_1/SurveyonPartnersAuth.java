c19bf6ccbfc195f0ff079951752accb1d50b72ea
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2015-02-26 10:39:59 +0900

    change IF

--- src/main/java/com/surveyon/partners/v1_1/SurveyonPartnersAuth.java
+++ src/main/java/com/surveyon/partners/v1_1/SurveyonPartnersAuth.java
@@ -3,6 +3,7 @@ package com.surveyon.partners.v1_1;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.crypto.Mac;
@@ -13,7 +14,7 @@ import org.apache.commons.codec.binary.Hex;
 public class SurveyonPartnersAuth {
 	private String query = null;
 	private String signature = null;
-	
+
 	public String getSignature() {
 		return signature;
 	}
@@ -21,41 +22,41 @@ public class SurveyonPartnersAuth {
 	public String getQuery() {
 		return query;
 	}
-	
-	public SurveyonPartnersAuth(Map params, String secret) throws Exception {		
+
+	public SurveyonPartnersAuth(LinkedHashMap params, String secret) throws Exception {		
 		this.generateSignature(params, secret);
 	}
-	
-	public static boolean verifySignature(Map params, String secret) throws Exception{
+
+	public static boolean verifySignature(LinkedHashMap params, String secret) throws Exception{
 		if (!params.containsKey("sig") || params.get("sig") == null){
 			throw new Exception("sig doesn't exist");
 		}
 		String signature =  params.get("sig").toString();
 		params.remove("sig");
-		
+
 		//create HTTP parameter string
 		String combined = SurveyonPartnersAuth.createCombinedParameter(params);
-		
+
 		//create sha256 Hex
 		String created= SurveyonPartnersAuth.creatHmacSha256Hex(combined, secret);
-		
+
 		return created.equals(signature);
 	}
 
-	private String generateSignature(Map params, String secret) throws Exception {
+	private String generateSignature(LinkedHashMap params, String secret) throws Exception {
 		//create HTTP parameter string
 		String combined = SurveyonPartnersAuth.createCombinedParameter(params);
-		
+
 		//create sha256 Hex
 		String sig = SurveyonPartnersAuth.creatHmacSha256Hex(combined, secret);
 		combined = combined + "&sig=" + sig;
-		
+
 		this.query = combined;
 		this.signature = sig;
 		return sig;
 	}
-	
-	private static String createCombinedParameter(Map params) throws Exception {
+
+	private static String createCombinedParameter(LinkedHashMap params) throws Exception {
 		StringBuilder builder = new StringBuilder();
 		for (Iterator it = params.entrySet().iterator(); it.hasNext();) {
 			Map.Entry entry = (Map.Entry)it.next();
@@ -69,7 +70,7 @@ public class SurveyonPartnersAuth {
 		builder.setLength(builder.length() - 1);
 		return builder.toString();
 	}
-	
+
 	private static String creatHmacSha256Hex(String message, String key) throws NoSuchAlgorithmException, InvalidKeyException {
 		SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(),"HmacMD5");
 		Mac mac = Mac.getInstance("HmacSHA256");
